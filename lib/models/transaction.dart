ddc1d902f23b371d7bde116714430265615d8034
jackson.comp1681@hotmail.com <jackson.comp1681@hotmail.com>
2022-04-07 18:31:54 -0300

    .

---
+++ lib/models/transaction.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: empty_constructor_bodies
+
+class Transaction {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
